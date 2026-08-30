d8f199eea17ba9150aa3f92d9bb90690cb85970e
haileybee <53802118+haileybee@users.noreply.github.com>
2026-08-30 11:49:54 -0600

    fix: disconnect veterans site from shared Supabase

--- site-config.js
+++ site-config.js
@@ -1,6 +1,6 @@
 window.SITE_CONFIG = Object.freeze({
-  SUPABASE_URL: 'https://zljduaahbyxnuglbugar.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_ZljzdOZd1LDQ5LX8z-5tNg_2SfCJGFc',
+  SUPABASE_URL: '',
+  SUPABASE_PUBLISHABLE_KEY: '',
   PAYPAL_CLIENT_ID: '',
   PAYPAL_CURRENCY: 'USD'
 });
